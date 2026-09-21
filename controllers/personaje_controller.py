07c1c76d13e7f8ed515649c25e45dcbd6fa1cd4e
Sergio Pastrana <spastranag@udistrital.edu.co>
2026-09-21 18:25:54 -0500

    Arreglo de error

--- controllers/personaje_controller.py
+++ controllers/personaje_controller.py
@@ -6,11 +6,12 @@ personaje_bp = Blueprint('personaje', __name__)
 
 lista_de_personajes = [
     Personaje("Atreuz", "Guerrero", 23, 100),
-    Personaje("Muad'Dib", "Profeta", 56, 100)
+    Personaje("Muad'Dib", "Mago", 56, 100)
 ]
 
 @personaje_bp.route('/', methods=['GET', 'POST'])
 def personajes():
+    error = None
     if request.method == 'POST':
         nombre = request.form.get('nombre')
         clase = request.form.get('clase')
@@ -22,11 +23,10 @@ def personajes():
 
         # Validación con la Capa de Negocio
         if not es_valido(nombre, clase, nivel):
-            return jsonify({"error": "Datos de personaje no válidos"}), 400
+            error = "Datos de personaje no válidos. Clases permitidas: Guerrero, Mago, Arquero."
+        else:
+            nuevo_personaje = Personaje(nombre, clase, nivel, 100)
+            lista_de_personajes.append(nuevo_personaje)
+            return redirect(url_for('personaje.personajes'))
 
-        nuevo_personaje = Personaje(nombre, clase, nivel, 100)
-        lista_de_personajes.append(nuevo_personaje)
-        
-        return redirect(url_for('personaje.personajes'))
-    
-    return render_template('personajes.html', lista_personajes=lista_de_personajes)
+    return render_template('personajes.html', lista_personajes=lista_de_personajes, error=error)
